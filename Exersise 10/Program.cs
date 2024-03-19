b99028833215d3fe9ef58d3b9a93f006ddeaef70
thomas3456787654 <thomas.hendrickson@s.capregboces.org>
2024-03-19 10:18:28 -0400

    Exercise 10 commit 1

---
+++ Exersise 10/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Exersise_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice = 0;
+
+            while (choice != 4)
+            {
+
+            //Print menu
+            Console.WriteLine("**************");
+            Console.WriteLine("Menu: ");
+            Console.WriteLine("1 - New Game");
+            Console.WriteLine("2 - Load Game");
+            Console.WriteLine("3 - Options");
+            Console.WriteLine("4 -  Quit");
+            Console.WriteLine("*");
+            Console.WriteLine();
+
+            //Ask the user for a choice
+            Console.WriteLine("Please choose a number from the menu (1, 2, 3, or 4) ");
+            choice = int.Parse(Console.ReadLine());
+            //Console.WriteLine(choice);
+
+            //print message using if statement
+            if (choice == 1)
+                {
+                    Console.WriteLine("Creating a new game... zzz");
+                }
+            else if (choice == 2);
+                {
+                    Console.WriteLine("Loading game... zzz");
+                }
+            else if (choice == 3);
+                {
+                    Console.WriteLine("Here are your options... /jk");
+                }
+            else if ( choice > 4 || choice)
+                
+            }
+        }
+    }
+}
